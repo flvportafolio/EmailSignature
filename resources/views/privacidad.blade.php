945a604f4b36ccb00c2f27a14ab7fecada4c5c53
flvportafolio <fabiolv2019@hotmail.com>
2020-10-06 18:28:41 -0400

    se actualizo las paginas de politica de privacidad y terminos del servicio

--- resources/views/privacidad.blade.php
+++ resources/views/privacidad.blade.php
@@ -48,7 +48,21 @@
         </div>
       </nav>
       <!-- END Navigation -->
-
+      <main role="main" class="container py-5">
+    
+      <h1>POLITICAS Y PRIVACIDAD</h1>
+      <div class="text-wrap pt-4">
+        <h4>Importante</h4>
+        <p>Email Signature no es 100% seguro, pero hacemos todo lo posible para brindarte las mejores condiciones de seguridad.</p>
+        <h4>Tus datos se mantendrán bajo estricta confidencialidad</h4>
+        <p>Email Signature usa un sistema de seguridad llamado SSL (Secure Socket Layer), que actualmente es el estándar usado por las compañías más importantes del mundo para realizar transacciones electrónicas seguras, lo que significa que toda tu información personal no podrá ser leída ni capturada por terceros mientras viaja por la red.</p>
+        <h4>Declaración de Privacidad</h4>
+        <p>Es importante que sepas que Email Signature no traspasará, bajo ninguna modalidad y a ninguna empresa que no pertenezca a Email Signature, datos personales de los clientes registrados en sus tiendas de Internet, y te asegura que estos serán manejados en forma absolutamente confidencial y conforme lo dispone la legislación vigente.</p>        
+        <h4>Links con otros sitios</h4>
+        <p>Este sitio puede contener "links" a otros sitios. Email Signature no es responsable de las prácticas de seguridad o privacidad, o el contenido de esos sitios. Asimismo, no avala ningún producto o servicio ofrecido en dichos sitios.</p>
+      </div>
+    
+    </main>   
       <!-- Pie de Pagina -->
       <footer class="container-fluid py-4 bg-footer">
         <div class="row">
--- resources/views/terminos.blade.php
+++ resources/views/terminos.blade.php
@@ -14,6 +14,12 @@
     {
       background: #f0f2f1;
     }
+    .parrafo 
+    {
+      text-align: justify;
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
     .bg-footer
     {
       background: #2E4053;
@@ -48,7 +54,40 @@
         </div>
       </nav>
       <!-- END Navigation -->
-      
+      <main role="main" class="container py-5">
+    
+    <h1>Terminos y Condiciones</h1>
+    <p class="parrafo">
+      <b>Email Signature</b> a través de este documento, comunica a nuestros Visitantes los términos y condiciones para la utilización y navegación en el sitio web.
+    </p>
+    <p class="parrafo">
+      El Usuario debe leer, entender y aceptar todas las condiciones establecidas en los Términos del Servicio y en las Políticas de Privacidad
+      El sólo uso del Sitio implica la aceptación irrevocable e incondicional por parte del Usuario de todos los términos y condiciones fijados en el presente, asumiendo la obligación de respetar las mismas. el Usuario se obligará a cumplir expresamente con las mismas, no pudiendo alegar el desconocimiento de tales Términos y Condiciones Generales y de la Política de Privacidad.
+    </p>
+    <h5>¿QUIÉNES PUEDEN USAR EL SERVICIO?</h5>
+    <p class="parrafo">
+      <h6>Requisitos de edad</h6>
+      Debes tener al menos 18 años para usar el Servicio.
+    </p>
+    <h5>USO DE LOS SERVICIOS</h5>
+    <p class="parrafo">
+      Usted no utilizará el Servicio para transmitir, mostrar, ejecutar o de algún modo poner a disposición mensajes, contenidos o materiales que sean ilegales, obscenos, amenazantes, masivos no solicitados o “spam”, difamatorios, invasores de la privacidad, o que violen o infrinjan derechos de autor, marcas registradas, patentes, secretos comerciales y otros derechos de propiedad intelectual, derechos de privacidad o publicidad, reglamentos o estatutos de comunicaciones, o cualesquiera otras leyes, incluyendo, sin limitación, las leyes sobre difamación, acoso y obscenidad.
+    </p>    
+    <h5>CAMBIOS</h5>
+    <p class="parrafo">
+    El administrador de Email Signature podrá modificar o reemplazar, en cualquier momento, los presentes términos y condiciones, en todo o en parte.
+    Es responsabilidad de los usuarios revisar estos Términos y Condiciones periódicamente para ver si hay cambios.
+    </p>
+    <h5>EXCENSIÓN DE RESPONSABILIDAD</h5>
+    <p class="parrafo">
+    Usted acepta eximir de responsabilidad al administrador de Email Signature de y contra cualquier reclamo y gasto, incluidos los honorarios de abogados, que surjan del uso de este Acuerdo, incluyendo pero no limitado a su violación de estos Términos y Condiciones.
+    </p>
+    <h5>RENUNCIA DE GARANTÍA</h5>
+    <p class="parrafo">
+    El administrador de Email Signature no ofrece ninguna garantía de que el sitio web estará libre de errores o que su acceso sea continuo o ininterrumpido.
+    </p>  
+    
+    </main>   
       <!-- Pie de Pagina -->
       <footer class="container-fluid py-4 bg-footer">
         <div class="row">
